c2385c604441b9a4ef121f11e58811a2d6efb5f8
stem-fresh <thestemfresh@gmail.com>
2024-09-04 08:35:56 +0530

    added

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,3 @@
-# main.tf
-
 provider "google" {
   project = var.project_id
   region  = var.region
@@ -100,62 +98,4 @@ resource "google_compute_instance" "vm_instance" {
 
     access_config {}
   }
-}
-
-resource "google_container_cluster" "primary" {
-  name     = var.gke_cluster_name
-  location = "us-central1-f"
-  network  = google_compute_network.vpc_network.name
-  subnetwork = google_compute_subnetwork.private_subnet.name
-  deletion_protection = false
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
-  }
-
-  private_cluster_config {
-    enable_private_endpoint = false
-    enable_private_nodes    = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = var.node_pool_name
-  location   = google_container_cluster.primary.location
-  cluster    = google_container_cluster.primary.name
-  node_count = 2
-
-  node_config {
-    machine_type = var.node_pool_machine_type
-    disk_size_gb = var.node_pool_disk_size
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-
-    tags = ["private-node"]
-
-    service_account = var.service_account_email
-  }
-
-  management {
-    auto_upgrade = true
-    auto_repair  = true
-  }
-}
-
-output "cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubernetes_endpoint" {
-  value = google_container_cluster.primary.endpoint
 }
