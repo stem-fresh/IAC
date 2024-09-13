a85fd53190e820f4736e68fc0166ec6cb1f27157
dineshsai14211 <dineshsai14211@gmail.com>
2024-09-13 10:40:57 +0530

    update

--- backend.tf
+++ backend.tf
@@ -1,3 +1,9 @@
+resource "google_storage_bucket" "my_bucket" {
+  name     = var.bucket_name
+  location = var.bucket_location
+  force_destroy = true # Allows deletion of bucket even if it contains objects
+}
+
 terraform {
   backend "gcs" {
     bucket  = var.bucket_name
--- bucket.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "my_bucket" {
-  name     = var.bucket_name
-  location = var.bucket_location
-  force_destroy = true # Allows deletion of bucket even if it contains objects
-}
