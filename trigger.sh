6d987f1e36d336b8f183961be20433e4ae7bce9b
stem-fresh <thestemfresh@gmail.com>
2024-10-13 16:19:59 +0530

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -4,6 +4,18 @@
 # -x Echo output to the stdout
 set -e -x
 
+# Check if terraform is installed
+if ! command -v terraform &> /dev/null; then
+  echo "Terraform not found, installing..."
+  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+  sudo apt-get update && sudo apt-get install -y terraform
+else
+  echo "Terraform is already installed"
+fi
+
+
 # Init the terraform
 terraform init
 
